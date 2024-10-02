2be2b2550deb70da38c668b852b2bae7f9769ac7
jayha <jayha@DESKTOP-8I0KI7V.dhcp.wustl.edu>
2024-10-02 15:51:49 -0500

    flagg pretty much finished

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,32 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		for (int i=0; i<255; i++) {
+			StdDraw.setPenColor(255-i,255,255);
+			double c = (.5/255);
+			StdDraw.filledRectangle(.5, (.5+c*(i)-.5*c), .5, .5*c);
+		}
+		
+		StdDraw.setPenColor(255,255,255);
+		StdDraw.filledRectangle(.5, .25, .25, .5);
+		
+		StdDraw.setPenColor(128,128,128);
+		StdDraw.filledRectangle(.5, .25, .20, .30);
+		
+		StdDraw.setPenColor(255,255,0);
+		
+		for (int i = 0; i<3; i++) {
+			for (int j = 0; j<4; j++) {
+				StdDraw.filledRectangle((.35 +.15*i), (.50 - .15*j), .01, .02);
+			}
+		}
+		
+		StdDraw.setPenColor(0,255,255);
+		for (int i = 0; i<4; i++) {
+			StdDraw.filledRectangle(.2+ .2*i, .1, .07, .01);
+		}
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,39 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		StdDraw.setPenColor(r,g,b);
+		boolean filled = in.nextBoolean();
+		
+		if (shape.equals("rectangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double hw = in.nextDouble();
+			double hh = in.nextDouble();
+			if (filled==true) {
+				StdDraw.filledRectangle(x, y, hw, hh);
+			} else {
+				StdDraw.rectangle(x,y,hw,hh);
+			}
+			
+			
+		} else if (shape.equals("ellipse")) {
+			double p = in.nextDouble();
+			double q = in.nextDouble();
+			double k = in.nextDouble();
+			double n = in.nextDouble();
+			if (filled==true) {
+				StdDraw.filledEllipse(p, q, k, n);
+			}
+			else {
+				StdDraw.ellipse(q, k, r, n);
+			
+		} else {
+			
+		}
+		
 	}
 }
